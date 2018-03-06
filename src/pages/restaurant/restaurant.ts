905386daee4d40ffd7a4e048f424b4f1d58a00ce
Michiel <michiel.leenknegt@student.howest.be>
2018-03-06 15:18:53 +0100

    camera werking
    Camera

--- src/pages/restaurant/restaurant.ts
+++ src/pages/restaurant/restaurant.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FireDataServiceProvider } from '../../providers/fire-data-service/fire-data-service';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the RestaurantPage page.
@@ -19,8 +20,15 @@ export class RestaurantPage {
     constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private db: FireDataServiceProvider
+    private db: FireDataServiceProvider,
+    private camera: Camera
   ) {
+  }
+    readonly options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
   }
 
   ionViewDidLoad() {
@@ -36,6 +44,15 @@ export class RestaurantPage {
     //   name:"New pizza store"
     // }
     // this.db.update("0",store);
+  }
+  takePicture(){
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
   }
 
 }
